1de1723e970aa2f8378f974265b73221d3ca7406
gaboemi <173064947+Gaboemi-Duoc@users.noreply.github.com>
2026-04-18 18:05:46 -0400

    Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+	[Header("Gravity")]
+	[HideInInspector] public float gravityStrength; //Downwards force (gravity) needed for the desired jumpHeight and jumpTimeToApex.
+	[HideInInspector] public float gravityScale; //Strength of the player's gravity as a multiplier of gravity (set in ProjectSettings/Physics2D).
+										  //Also the value the player's rigidbody2D.gravityScale is set to.
+	[Space(5)]
+	public float fallGravityMult; //Multiplier to the player's gravityScale when falling.
+	public float maxFallSpeed; //Maximum fall speed (terminal velocity) of the player when falling.
+	[Space(5)]
+	public float fastFallGravityMult; //Larger multiplier to the player's gravityScale when they are falling and a downwards input is pressed.
+									  //Seen in games such as Celeste, lets the player fall extra fast if they wish.
+	public float maxFastFallSpeed; //Maximum fall speed(terminal velocity) of the player when performing a faster fall.
+	
+	[Space(20)]
+
     [Header("Run")]
 	public float runMaxSpeed; //Target speed we want the player to reach.
 	public float runAcceleration; //Time (approx.) time we want it to take for the player to accelerate from 0 to the runMaxSpeed.
@@ -13,29 +27,69 @@ public class PlayerController : MonoBehaviour
 	[Range(0.01f, 1)] public float accelInAir; //Multipliers applied to acceleration rate when airborne.
 	[Range(0.01f, 1)] public float deccelInAir;
 	public bool doConserveMomentum;
-    public Rigidbody2D rb2d { get; private set; }
 
+	[Space(20)]
+
+	[Header("Jump")]
+	public float jumpHeight; //Height of the player's jump
+	public float jumpTimeToApex; //Time between applying the jump force and reaching the desired jump height. These values also control the player's gravity and jump force.
+	[HideInInspector] public float jumpForce; //The actual force applied (upwards) to the player when they jump.
+
+	[Header("Both Jumps")]
+	public float jumpCutGravityMult; //Multiplier to increase gravity if the player releases thje jump button while still jumping
+	[Range(0f, 1)] public float jumpHangGravityMult; //Reduces gravity while close to the apex (desired max height) of the jump
+	public float jumpHangTimeThreshold; //Speeds (close to 0) where the player will experience extra "jump hang". The player's velocity.y is closest to 0 at the jump's apex (think of the gradient of a parabola or quadratic function)
+	[Space(0.5f)]
+	public float jumpHangAccelerationMult; 
+	public float jumpHangMaxSpeedMult; 				
+
+	[Header("Wall Jump")]
+	public Vector2 wallJumpForce; //The actual force (this time set by us) applied to the player when wall jumping.
+	[Space(5)]
+	[Range(0f, 1f)] public float wallJumpRunLerp; //Reduces the effect of player's movement while wall jumping.
+	[Range(0f, 1.5f)] public float wallJumpTime; //Time after wall jumping the player's movement is slowed for.
+	public bool doTurnOnWallJump; //Player will rotate to face wall jumping direction
     #region COMPONENTS
-    public Rigidbody2D RB { get; private set; }
 	//Script to handle all player animations, all references can be safely removed if you're importing into your own project.
 	#endregion
+    [Header("Assists")]
+	[Range(0.01f, 0.5f)] public float coyoteTime; //Grace period after falling off a platform, where you can still jump
+	[Range(0.01f, 0.5f)] public float jumpInputBufferTime; //Grace period after pressing jump where a jump will be automatically performed once the requirements (eg. being grounded) are met.
 
 	#region STATE PARAMETERS
 	//Variables control the various actions the player can perform at any time.
 	//These are fields which can are public allowing for other sctipts to read them
 	//but can only be privately written to.
 	public bool IsFacingRight { get; private set; }
+	public bool IsJumping { get; private set; }
+	public bool IsWallJumping { get; private set; }
 	public float LastOnGroundTime { get; private set; }
+	public float LastOnWallTime { get; private set; }
+	public float LastOnWallRightTime { get; private set; }
+	public float LastOnWallLeftTime { get; private set; }
+	
+	//Jump
+	private bool _isJumpCut;
+	private bool _isJumpFalling;
+
+	//Wall Jump
+	private float _wallJumpStartTime;
+	private int _lastWallJumpDir;
 	#endregion
 
 	#region INPUT PARAMETERS
     private Vector2 _moveInput;
+
+	public float LastPressedJumpTime { get; private set; }
 	#endregion
 
 	#region CHECK PARAMETERS
 	//Set all of these up in the inspector
 	[Header("Checks")] 
 	[SerializeField] private Transform _groundCheckPoint;
+	[SerializeField] private Transform _frontWallCheckPoint;
+	[SerializeField] private Transform _backWallCheckPoint;
+	[SerializeField] private Vector2 _wallCheckSize = new Vector2(0.5f, 1f);
 	[SerializeField] private Vector2 _groundCheckSize = new Vector2(0.49f, 0.03f);
 	#endregion
 
@@ -45,24 +99,38 @@ public class PlayerController : MonoBehaviour
 	#endregion
 
     private InputAction moveAction;
+	private InputAction jumpAction;
+    public Rigidbody2D rb2d { get; private set; }
 
     void Start()
     {
 		IsFacingRight = true;
+		IsJumping = false;
     }
 
     void Awake()
     {
         // Busca la acción "Move" dentro del InputSystem_Actions
         moveAction = InputSystem.actions.FindAction("Move");
+		jumpAction = InputSystem.actions.FindAction("Jump");
         rb2d = GetComponent<Rigidbody2D>();
     }
 
 	private void OnValidate()
     {
+		//Calculate gravity strength using the formula (gravity = 2 * jumpHeight / timeToJumpApex^2) 
+		gravityStrength = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
+		//Calculate the rigidbody's gravity scale (ie: gravity strength relative to unity's gravity value, see project settings/Physics2D)
+		gravityScale = gravityStrength / Physics2D.gravity.y;
+		print(gravityStrength);
+		print(Physics2D.gravity.y);
+
 		//Calculate are run acceleration & deceleration forces using formula: amount = ((1 / Time.fixedDeltaTime) * acceleration) / runMaxSpeed
 		runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
 		runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
+
+		//Calculate jumpForce using the formula (initialJumpVelocity = gravity * timeToJumpApex)
+		jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
 
 		#region Variable Ranges
 		runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
@@ -83,27 +151,164 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _moveInput = moveAction.ReadValue<Vector2>();
 		#region TIMERS
         LastOnGroundTime -= Time.deltaTime;
+
+		LastOnWallTime -= Time.deltaTime;
+		LastOnWallRightTime -= Time.deltaTime;
+		LastOnWallLeftTime -= Time.deltaTime;
+
+		LastPressedJumpTime -= Time.deltaTime;
 		#endregion
 
 		#region INPUT HANDLER
+        _moveInput = moveAction.ReadValue<Vector2>();
+		
+
 		if (_moveInput.x != 0)
 			CheckDirectionToFace(_moveInput.x > 0);
 		#endregion
 
+		if (jumpAction.WasPressedThisFrame())
+		{
+			OnJumpInput();
+		}
+		if (jumpAction.WasReleasedThisFrame())
+		{
+			OnJumpUpInput();
+		}
+
 		#region COLLISION CHECKS
-		//Ground Check
-		if (Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer)) //checks if set box overlaps with ground
-			LastOnGroundTime = 0.1f;
+		if (!IsJumping)
+		{
+			//Ground Check
+			if (Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer)) //checks if set box overlaps with ground
+			{
+				if(LastOnGroundTime < -0.1f)
+                {
+					//AnimHandler.justLanded = true;
+                }
+
+				LastOnGroundTime = coyoteTime; //if so sets the lastGrounded to coyoteTime
+            }		
+
+			//Right Wall Check
+			if (((Physics2D.OverlapBox(_frontWallCheckPoint.position, _wallCheckSize, 0, _groundLayer) && IsFacingRight)
+					|| (Physics2D.OverlapBox(_backWallCheckPoint.position, _wallCheckSize, 0, _groundLayer) && !IsFacingRight)) && !IsWallJumping)
+				LastOnWallRightTime = coyoteTime;
+
+			//Right Wall Check
+			if (((Physics2D.OverlapBox(_frontWallCheckPoint.position, _wallCheckSize, 0, _groundLayer) && !IsFacingRight)
+				|| (Physics2D.OverlapBox(_backWallCheckPoint.position, _wallCheckSize, 0, _groundLayer) && IsFacingRight)) && !IsWallJumping)
+				LastOnWallLeftTime = coyoteTime;
+
+			//Two checks needed for both left and right walls since whenever the play turns the wall checkPoints swap sides
+			LastOnWallTime = Mathf.Max(LastOnWallLeftTime, LastOnWallRightTime);
+		}
+		#endregion
+		#region JUMP CHECKS
+		if (IsJumping && rb2d.linearVelocityY < 0)
+		{
+			IsJumping = false;
+
+			_isJumpFalling = true;
+		}
+
+		if (IsWallJumping && Time.time - _wallJumpStartTime > wallJumpTime)
+		{
+			IsWallJumping = false;
+		}
+
+		if (LastOnGroundTime > 0 && !IsJumping && !IsWallJumping)
+        {
+			_isJumpCut = false;
+
+			_isJumpFalling = false;
+		}
+		if (CanJump() && LastPressedJumpTime > 0)
+		{
+			IsJumping = true;
+			IsWallJumping = false;
+			_isJumpCut = false;
+			_isJumpFalling = false;
+			Jump();
+
+			//AnimHandler.startedJumping = true;
+		}
+		//WALL JUMP
+		else if (CanWallJump() && LastPressedJumpTime > 0)
+		{
+			IsWallJumping = true;
+			IsJumping = false;
+			_isJumpCut = false;
+			_isJumpFalling = false;
+
+			_wallJumpStartTime = Time.time;
+			_lastWallJumpDir = (LastOnWallRightTime > 0) ? -1 : 1;
+
+			WallJump(_lastWallJumpDir);
+		}
+		#endregion
+		#region GRAVITY
+		//Higher gravity if we've released the jump input or are falling
+		if (rb2d.linearVelocity.y < 0 && _moveInput.y < 0)
+		{
+			//Much higher gravity if holding down
+			SetGravityScale(gravityScale * fastFallGravityMult);
+			//Caps maximum fall speed, so when falling over large distances we don't accelerate to insanely high speeds
+			rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, Mathf.Max(rb2d.linearVelocityY, -maxFastFallSpeed));
+		}
+		else if (_isJumpCut)
+		{
+			//Higher gravity if jump button released
+			SetGravityScale(gravityScale * jumpCutGravityMult);
+			rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, Mathf.Max(rb2d.linearVelocityY, -maxFallSpeed));
+		}
+		else if ((IsJumping || IsWallJumping || _isJumpFalling) && Mathf.Abs(rb2d.linearVelocityY) < jumpHangTimeThreshold)
+		{
+			SetGravityScale(gravityScale * jumpHangGravityMult);
+		}
+		else if (rb2d.linearVelocityY < 0)
+		{
+			//Higher gravity if falling
+			SetGravityScale(gravityScale * fallGravityMult);
+			//Caps maximum fall speed, so when falling over large distances we don't accelerate to insanely high speeds
+			rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, Mathf.Max(rb2d.linearVelocity.y, -maxFallSpeed));
+		}
+		else
+		{
+			//Default gravity if standing on a platform or moving upwards
+			SetGravityScale(gravityScale);
+		}
 		#endregion
     }
 
     void FixedUpdate()
     {
         Run();
+
+		if(jumpAction.IsPressed() && !IsJumping && LastOnGroundTime > 0)
+		{
+			Jump();
+		}
     }
+	
+	//Methods which whandle input detected in Update()
+    public void OnJumpInput()
+	{
+		LastPressedJumpTime = jumpInputBufferTime;
+	}
+
+	public void OnJumpUpInput()
+	{
+		if (CanJumpCut() || CanWallJumpCut())
+			_isJumpCut = true;
+	}
+
+    public void SetGravityScale(float scale)
+	{
+		rb2d.gravityScale = scale;
+	}
 
 	//MOVEMENT METHODS
     #region RUN METHODS
@@ -122,18 +327,17 @@ public class PlayerController : MonoBehaviour
 		else
 			accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? runAccelAmount * accelInAir : runDeccelAmount * deccelInAir;
 		#endregion
-		print(runAccelAmount);
 
 		//Not used since no jump implemented here, but may be useful if you plan to implement your own
 
-		// #region Add Bonus Jump Apex Acceleration
-		// //Increase are acceleration and maxSpeed when at the apex of their jump, makes the jump feel a bit more bouncy, responsive and natural
-		// if ((IsJumping || IsWallJumping || _isJumpFalling) && Mathf.Abs(RB.velocity.y) < jumpHangTimeThreshold)
-		// {
-		// 	accelRate *= jumpHangAccelerationMult;
-		// 	targetSpeed *= jumpHangMaxSpeedMult;
-		// }
-		// #endregion
+		#region Add Bonus Jump Apex Acceleration
+		//Increase are acceleration and maxSpeed when at the apex of their jump, makes the jump feel a bit more bouncy, responsive and natural
+		if ((IsJumping || IsWallJumping || _isJumpFalling) && Mathf.Abs(rb2d.linearVelocity.y) < jumpHangTimeThreshold)
+		{
+			accelRate *= jumpHangAccelerationMult;
+			targetSpeed *= jumpHangMaxSpeedMult;
+		}
+		#endregion
 
 		//Calculate difference between current linearVelocity and desired linearVelocity
 		float speedDif = targetSpeed - rb2d.linearVelocityX;
@@ -145,6 +349,44 @@ public class PlayerController : MonoBehaviour
 		// rb2d.AddForce( Vector2.right, ForceMode2D.Force);
 		rb2d.AddForceX(movement, ForceMode2D.Force);
     }
+
+	private void Jump()
+	{
+		LastPressedJumpTime = 0;
+		LastOnGroundTime = 0;
+
+		float force = jumpForce;
+		IsJumping = true;
+		if (rb2d.linearVelocityY < 0)
+			force -= rb2d.linearVelocityY; 
+
+		rb2d.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        //playeranim.SetTrigger("jump");
+	}
+
+	
+	private void WallJump(int dir)
+	{
+		//Ensures we can't call Wall Jump multiple times from one press
+		LastPressedJumpTime = 0;
+		LastOnGroundTime = 0;
+		LastOnWallRightTime = 0;
+		LastOnWallLeftTime = 0;
+
+		#region Perform Wall Jump
+		Vector2 force = new Vector2(wallJumpForce.x, wallJumpForce.y);
+		force.x *= dir; //apply force in opposite direction of wall
+
+		if (Mathf.Sign(rb2d.linearVelocityX) != Mathf.Sign(force.x))
+			force.x -= rb2d.linearVelocityX;
+
+		if (rb2d.linearVelocityY < 0) //checks whether player is falling, if so we subtract the velocity.y (counteracting force of gravity). This ensures the player always reaches our desired jump force or greater
+			force.y -= rb2d.linearVelocityY;
+
+		rb2d.AddForce(force, ForceMode2D.Impulse);
+		#endregion
+	}
+
 	private void Turn()
 	{
 		//stores scale and flips the player along the x axis, 
@@ -162,6 +404,37 @@ public class PlayerController : MonoBehaviour
 	{
 		if (isMovingRight != IsFacingRight)
 			Turn();
+	}
+    private bool CanJump()
+    {
+		return LastOnGroundTime > 0 && !IsJumping;
+    }
+
+	private bool CanWallJump()
+    {
+		return LastPressedJumpTime > 0 && LastOnWallTime > 0 && LastOnGroundTime <= 0 && (!IsWallJumping ||
+			 (LastOnWallRightTime > 0 && _lastWallJumpDir == 1) || (LastOnWallLeftTime > 0 && _lastWallJumpDir == -1));
+	}
+
+	private bool CanJumpCut()
+    {
+		return IsJumping && rb2d.linearVelocityY > 0;
+    }
+
+	private bool CanWallJumpCut()
+	{
+		return IsWallJumping && rb2d.linearVelocityY > 0;
+	}
+    #endregion
+
+	#region EDITOR METHODS
+    private void OnDrawGizmosSelected()
+    {
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(_groundCheckPoint.position, _groundCheckSize);
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireCube(_frontWallCheckPoint.position, _wallCheckSize);
+		Gizmos.DrawWireCube(_backWallCheckPoint.position, _wallCheckSize);
 	}
     #endregion
 }
